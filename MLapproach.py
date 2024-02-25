df1d94af1f1de08c93f9455fe227864915ca631f
a-rea <area@chapman.edu>
2024-02-25 15:30:56 -0800

    pushing the RF, CNN, and gradcam scripts

---
+++ MLapproach.py
@@ -0,0 +1,210 @@
+import os
+from PIL import Image
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import seaborn as sns
+
+# function to calculate statistics from an image
+def calculate_image_stats(image):
+    # convert the image to grayscale
+    grayscale_image = image.convert('L')
+    
+    # grey scale image to array to calculate statistics
+    image_array = np.array(grayscale_image)
+    
+    # gather image statisttics as features for a dataframe
+    mean_intensity = np.mean(image_array)
+    median_intensity = np.median(image_array)
+    std_intensity = np.std(image_array)
+    min_intensity = np.min(image_array)
+    max_intensity = np.max(image_array)
+    intensity_ratio = max_intensity / min_intensity
+    max_variance = max_intensity - min_intensity
+    IQRIntensity = np.percentile(image_array, 75) - np.percentile(image_array, 25)
+
+    return {
+        'Mean Intensity': mean_intensity,
+        'Median Intensity': median_intensity,
+        'Std Intensity': std_intensity,
+        'Min Intensity': min_intensity,
+        'Max Intensity': max_intensity,
+        'Intensity Ratio': intensity_ratio,
+        'Max Variance': max_variance,
+        'IQR Intensity': IQRIntensity
+    }
+
+# extract statistics from images in a directory
+def extract_stats_from_images(directory):
+    image_stats = []
+    
+    # iterate through each image file in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg"):
+            filepath = os.path.join(directory, filename)
+            try:
+                # open the image using Pillow
+                image = Image.open(filepath)
+                #print(f"Processing image: {filename}")
+                # calculate statistics for the image
+                stats = calculate_image_stats(image)
+                # get the class of each image
+                stats['label'] = directory.split('/')[-1]
+                image_stats.append(stats)
+            except Exception as e:
+                print(f"Error processing image {filename}: {e}")
+    
+    # convert the list of dictionaries into a DataFrame
+    df = pd.DataFrame(image_stats)
+    return df
+
+
+def construct_dataframes():
+    # string paths to the directories containing the images
+    train_fake_stats = 'train/FAKE'
+    train_real_stats = 'train/REAL'
+    test_fake_stats = 'test/FAKE'
+    test_real_stats = 'test/REAL'
+
+    # extract statistics from images in the directory
+    train_fake_img_stats = extract_stats_from_images(train_fake_stats)
+    train_real_img_stats = extract_stats_from_images(train_real_stats)
+    test_fake_img_stats = extract_stats_from_images(test_fake_stats)
+    test_real_img_stats = extract_stats_from_images(test_real_stats)
+
+    # combine all the img stats into one dataframe
+    train_image_stats_df = pd.concat([train_fake_img_stats, train_real_img_stats])
+    test_image_stats_df = pd.concat([test_fake_img_stats, test_real_img_stats]) 
+
+    # ensure there are no infinities or NaN values
+    train_image_stats_df = train_image_stats_df.replace([np.inf, -np.inf], np.nan).dropna()
+    test_image_stats_df = test_image_stats_df.replace([np.inf, -np.inf], np.nan).dropna()
+
+    # encode the label column to be 0 for FAKE and 1 for REAL
+    train_image_stats_df['label'] = train_image_stats_df['label'].map({'FAKE': 0, 'REAL': 1})
+    test_image_stats_df['label'] = test_image_stats_df['label'].map({'FAKE': 0, 'REAL': 1})
+
+
+    # save the dataframe to a csv file
+    train_image_stats_df.to_csv('image_stats_train.csv', index=False)
+    test_image_stats_df.to_csv('image_stats_test.csv', index=False)
+
+    return train_image_stats_df, test_image_stats_df
+
+
+
+def exploratory_data_analysis():
+    train_data = pd.read_csv('image_stats_train.csv')
+
+    # Exploratory Data Analysis
+
+    corr = train_data.corr()
+    plt.figure(figsize=(12, 12))
+    #plot heatmap of the correlation matrix
+    sns.heatmap(corr, cmap='coolwarm')
+    plt.title("Image Statistics Correlation Matrix")
+    # show the plot
+    plt.show()
+    plt.savefig('RF_images/correlation_matrix.png')
+
+
+    # plot histograms of the image statistics
+    plt.figure()
+    train_data.hist(figsize=(12, 12))
+    plt.title("Image Statistics Histogram")
+    plt.savefig('RF_images/train_histogram.png')
+
+
+def hyperparameter_tuning(train_image_stats_df, flag):
+    if flag:
+        # define the features and the target
+        X_train = train_image_stats_df.drop(columns=['label'])
+        y_train = train_image_stats_df['label']
+
+        param_grid = { 
+            'n_estimators': [50, 100, 200],
+            'max_features': ['auto', 'sqrt'],
+            'max_depth': [None, 10, 20],
+            'min_samples_split': [2, 5],
+            'min_samples_leaf': [1, 2],
+            'bootstrap': [True, False],
+            'criterion': ['gini', 'entropy'] 
+        }   
+        
+        grid_search = GridSearchCV(RandomForestClassifier(), 
+                            param_grid=param_grid) 
+        grid_search.fit(X_train, y_train) 
+        print(grid_search.best_estimator_)
+        print(grid_search.best_params_)
+        print(grid_search.best_score_)
+
+        return grid_search.best_estimator_
+    
+
+
+# function to create random forest model that uses the image_stats_df to predict if an image is real or fake
+def define_RF(train_image_stats_df, test_image_stats_df):
+
+    # define the features and the target
+    X_train = train_image_stats_df.drop(columns=['label'])
+    y_train = train_image_stats_df['label']
+
+    X_test = test_image_stats_df.drop(columns=['label'])
+    y_test = test_image_stats_df['label']
+    
+    # define the model
+    model = RandomForestClassifier()
+    
+    # train the model
+    model.fit(X_train, y_train)
+    
+    # evaluate the model
+    train_score = model.score(X_train, y_train)
+    test_score = model.score(X_test, y_test)
+
+
+    print(f"Train score: {train_score:.2f}")
+    print(f"Test score: {test_score:.2f}")
+
+    y_pred_proba = model.predict_proba(X_test)[:,1]
+    fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+    auc = metrics.roc_auc_score(y_test, y_pred_proba)
+
+    # create ROC curve with white background and grid
+    plt.figure()
+    plt.plot(fpr,tpr,label="AUC="+str(auc))
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Random guess')
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.grid()
+    plt.legend(loc=4)
+    plt.savefig("RF_images/roc_curve.png")
+
+    #confusion matrix of the model
+    y_pred = model.predict(X_test)
+    confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
+    plt.figure()
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,
+                                  display_labels=model.classes_)
+    disp.plot()
+    plt.show()
+    plt.savefig("RF_images/RF_confusion_matrix.png")
+    
+    return model
+
+
+if __name__ == "__main__":
+    # define a random forest model
+
+    train_data, test_data = construct_dataframes()
+    model = hyperparameter_tuning(train_data, False)
+    exploratory_data_analysis()
+    model = define_RF(train_data, test_data)
+
+
+
+    
+
---
+++ assignment1.py
@@ -0,0 +1,179 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+import tensorflow.keras as kb
+from tensorflow.keras import backend
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, MaxPooling2D, Conv2D, BatchNormalization, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+
+train_folder = "train"
+validation_folder = "test"
+
+def data_processing():
+    # load the images and create
+    train = ImageDataGenerator(rescale=1/255, shear_range=0.2).flow_from_directory(train_folder,
+                                                                  target_size=(32,32),                                                             
+                                                                  batch_size=32,
+                                                                  class_mode='binary')
+
+    test = ImageDataGenerator(rescale=1/255).flow_from_directory(validation_folder,
+                                                                    target_size=(32,32),                                                             
+                                                                    batch_size=32,
+                                                                    class_mode='binary')
+    return train, test
+
+
+def construct_model():
+  cnn = kb.Sequential()
+
+  # this is the first block (conv layer + max pooling + batch norm + dropout)
+  cnn.add(Conv2D(64, (2,2), activation = "relu", padding = "same", input_shape=(32, 32,3)))
+  cnn.add(Conv2D(64, (2,2), activation = "relu", padding = "same"))
+  cnn.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+  cnn.add(BatchNormalization())
+  cnn.add(Dropout(0.4))
+
+  # second block but now with more filters 
+  cnn.add(Conv2D(128, (2,2), activation = "relu", padding = "same"))
+  cnn.add(Conv2D(128, (2,2), activation = "relu", padding = "same"))
+  cnn.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+  cnn.add(BatchNormalization())
+  cnn.add(Dropout(0.4))
+
+  # third block with even MORE filters 
+  cnn.add(Conv2D(256, (2,2), activation = "relu", padding = "same"))
+  cnn.add(Conv2D(256, (2,2), activation = "relu", padding = "same"))
+  cnn.add(Conv2D(256, (2,2), activation = "relu", padding = "same"))
+  cnn.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+  cnn.add(BatchNormalization())
+  cnn.add(Dropout(0.3))
+
+  # fourth block with even even more filters 
+  cnn.add(Conv2D(512, (2,2), activation = "relu", padding = "same"))
+  cnn.add(Conv2D(512, (2,2), activation = "relu", padding = "same"))
+  cnn.add(Conv2D(512, (2,2), activation = "relu", padding = "same"))
+  cnn.add(MaxPooling2D(pool_size=(2, 2), padding = "same"))
+  cnn.add(BatchNormalization())
+  cnn.add(Dropout(0.2))
+
+  # flatten and ffnn portion
+  cnn.add(Flatten())
+  cnn.add(Dense(4000, activation='relu'))
+  cnn.add(Dense(1500, activation='relu'))
+  cnn.add(Dense(1, activation='sigmoid')) #predict our 2 categories
+
+  cnn.summary()
+
+  return cnn
+
+def compile_and_fit(model, train, test, ep, batch_size=32):
+    model.compile(optimizer=Adam(learning_rate=0.0005),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy', kb.metrics.Precision(), kb.metrics.Recall()])
+    history = model.fit(train, epochs=ep, batch_size=batch_size, validation_data=test)
+    model.save('CIFAKE_cnn.h5')
+
+    #Evaluaion of the model
+    print("PRINTING THE EVALUATION OF THE MODEL")
+    model.evaluate(test)
+
+    return history
+
+
+
+'''two functions to plot the loss and accuracy of the models'''
+def plot_loss(res):
+  plt.figure(figsize=(10, 10))
+  plt.plot(res.history["loss"], label = "train loss")
+  plt.plot(res.history["val_loss"], label = "validation loss")
+  plt.title("Categorical Cross Entropy per Epoch")
+  plt.xlabel("Epochs passed")
+  plt.ylabel("Categorical Cross Entropy")
+  plt.legend()
+  plt.savefig('CNN_images/lossPerEpoch350.png')
+  plt.show()
+
+
+
+def plot_accuracy(res):
+  plt.figure(figsize=(10, 10))
+  plt.plot(res.history["accuracy"], label = "train accuracy")
+  plt.plot(res.history["val_accuracy"], label = "validation accuracy")
+  plt.title("Accuracy per Epoch")
+  plt.xlabel("Epochs passed")
+  plt.ylabel("Accuracy")
+  plt.legend()
+  plt.savefig('CNN_images/accuracyPerEpoch350.png')
+  plt.show()
+
+
+
+''' plotting the results of our predicions of what our model predicts vs true class '''
+def plot_predictions(cnn, test):
+    # get some test images
+
+    cnn = kb.models.load_model('CIFAKE_cnn.h5')
+    
+    images, labels = next(test)
+    images = images / np.max(images) * 255
+
+    # make predictions
+    predictions = cnn.predict(test)
+
+    # visualize predictions
+    _, axes = plt.subplots(nrows=4, ncols=4, figsize=(10, 10))
+
+    for i, ax in enumerate(axes.flat):
+        # plot image
+        ax.imshow(images[i].astype('uint8'))
+        ax.axis('off')
+
+        # set title to predicted class and true class
+        pred_class = 1 if predictions[i] > 0.5 else 0
+        print("PRED CLASS", pred_class)
+        true_class = labels[i]
+        ax.set_title(f"Pred: {pred_class}, True: {true_class}",
+                    color=("green" if pred_class == true_class else "red"))
+
+    plt.savefig("CNN_images/predVStrue350.png")
+
+
+
+def plot_confusion_matrix(model, test):
+    # create and plot confusion matrix
+    predictions = model.predict(test)
+    y_pred = (predictions > 0.6).astype(int)  # Thresholding at 0.6
+    confusion_matrix = metrics.confusion_matrix(test.classes, y_pred)
+    accuracy = np.trace(confusion_matrix) / np.sum(confusion_matrix)
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix)
+    disp.plot()
+    plt.xlabel(f"Accuracy: {accuracy:.2f}")
+    plt.show()
+    plt.savefig("CNN_images/cnn_confusion_matrix350.png")
+    print(confusion_matrix)
+    
+
+if __name__ == "__main__":
+    train, test = data_processing()
+    cnn = construct_model()
+    history = compile_and_fit(cnn, train, test, ep=350, batch_size=32)
+    plot_loss(history)
+    plot_accuracy(history)
+    plot_predictions(cnn, test)
+    plot_confusion_matrix(cnn, test)
+
+'''
+useful links 
+
+https://www.tensorflow.org/api_docs/python/tf/keras/applications/ResNet50
+https://medium.com/@kenneth.ca95/a-guide-to-transfer-learning-with-keras-using-resnet50-a81a4a28084b
+
+'''
